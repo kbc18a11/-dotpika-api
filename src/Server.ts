c5de5a0147dd88610c1a0d4ea79a101ecd080633
wadanaoumi <wada121400@gmail.com>
2021-06-09 11:48:16 +0900

    corsに対応

--- src/Server.ts
+++ src/Server.ts
@@ -6,6 +6,7 @@ import helmet from 'helmet';
 import express, { NextFunction, Request, Response } from 'express';
 import StatusCodes from 'http-status-codes';
 import 'express-async-errors';
+import cors from 'cors';
 
 import BaseRouter from './routes';
 import logger from '@shared/Logger';
@@ -20,8 +21,9 @@ const { BAD_REQUEST } = StatusCodes;
  ***********************************************************************************/
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors());
 
 // Show routes called in console during development
 if (process.env.NODE_ENV === 'development') {
@@ -56,7 +58,7 @@ app.set('views', viewsDir);
 const staticDir = path.join(__dirname, 'public');
 app.use(express.static(staticDir));
 app.get('*', (req: Request, res: Response) => {
-    res.sendFile('index.html', {root: viewsDir});
+    res.sendFile('index.html', { root: viewsDir });
 });
 
 // Export express instance
